e59a3fb0e037d3664bbc9d9bc23a5e5d6c348ba3
David111922 <salve62@yahoo.com>
2024-06-12 00:04:53 -0700

    eliminated search for term

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import { startTransition, useEffect, useState } from 'react';
-import Gallery from './component/gallery';
-import SearchBar from './component/searchBar';
-import "./App.css"
+import { useEffect, useState, useRef } from "react";
+import Gallery from "./component/gallery";
+import SearchBar from "./component/searchBar";
+import { SearchContext } from "./context/searchContext";
+import { DataContext } from "./context/DataContext";
 
-function App () {
-    const [search, setSearch] = useState('');
-    const [message, setMessage] = useState('Search for Music!');
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            document.title = `${search} Music`
-            const response = await fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(search)}`)
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-              setData(resData.results)
-            } else {
-              setMessage('Not Found')
-            }
-          } catch (error) {
-            console.error("Fetch error: ", error);
-            setMessage('Failed to fetch data')
-          }
-        }
-        if (search) {
-          fetchData()
-        }
-      }, [search])
-    
+function App() {
+  const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("Search for Music!");
+  const searchInput = useRef("");
+  const [data, setData] = useState([]);
   
-    const handleSearch = (e, term) => {
-        e.preventDefault();
-        setSearch(term);
+
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    const fetchData = async () => {
+      try {
+        document.title = `${term} Music`;
+        const response = await fetch(
+          `https://itunes.apple.com/search?term=${encodeURIComponent(term)}`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const resData = await response.json();
+        if (resData.results.length > 0) {
+          setData(resData.results);
+        } else {
+          setMessage("Not Found");
+        }
+      } catch (error) {
+        console.error("Fetch error: ", error);
+        setMessage("Failed to fetch data");
+      }
     };
+    if (term) {
+      fetchData();
+    }
+  };
 
-    return (
+  return (
+    <div className="App">
+      <SearchContext.Provider
+        value={{
+          term: searchInput,
+          handleSearch: handleSearch,
+        }}
+      >
+        <SearchBar />
+      </SearchContext.Provider>
+      {message}
 
-        <div>
-
-            <SearchBar handleSearch={handleSearch} />
-            {message && <p>{message}</p>}
-            <Gallery data={data} />
-        </div>
-    );
-};
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
+    </div>
+  );
+}
 
 export default App;
