bbdad959a421337fc82a59e61c69160d100b242a
David111922 <salve62@yahoo.com>
2024-06-19 23:36:35 -0700

    working with suspense

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Gallery from './component/gallery'
 import SearchBar from './component/searchBar'
 import AlbumView from './component/AlbumView'
 import ArtistView from './component/ArtistView'
+import { createResource as fetchData } from './helper'
 
 function App() {
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+    let [data, setData] = useState(null)
 
     const API_URL = 'https://itunes.apple.com/search?term='
 
     useEffect(() => {
         if(search) {
+            // setData (fetchData(search))
             const fetchData = async () => {
                 document.title = `${search} Music`
                 const response = await fetch(API_URL + search)
@@ -26,6 +28,7 @@ function App() {
             }
             fetchData()
         }
+        // error searchTerm is not defined I changed it from search to searchTerm.I reverted back to search
     }, [search])
     
     const handleSearch = (e, term) => {
@@ -41,7 +44,7 @@ function App() {
                   <Route path="/" element={
                       <>
                           <SearchBar handleSearch = {handleSearch}/>
-                          <Gallery data={data} />
+                          {/* <Gallery data={data} /> */}
                       </>
                   } />
                   <Route path="/album/:id" element={<AlbumView />} />
